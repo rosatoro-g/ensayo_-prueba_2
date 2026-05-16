3b9e1f23c1d19ae54003380d80edd7908f71fd72
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-15 21:12:34 -0400

    tercer

--- src/main/java/cl/app/ms_products_db/Controller/ProductController.java
+++ src/main/java/cl/app/ms_products_db/Controller/ProductController.java
@@ -3,21 +3,34 @@ package cl.app.ms_products_db.Controller;
 import cl.app.ms_products_db.Service.ProductService;
 import cl.app.ms_products_db.model.Producto;
 import cl.app.ms_products_db.model.dto.ProductDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@Slf4j // Registro de cada acciom que se hace para el programa
 public class ProductController {
     @Autowired
     ProductService productService;
     @GetMapping()
     public ResponseEntity<List<ProductDto>>findAll(){
-        return ResponseEntity.ok(productService.findAll());
+        log.info("Request findAll()");
+        List<ProductDto>lista = productService.findAll();
+        log.info("Response findAll():{}",lista);
+        return ResponseEntity.ok(lista);
+
+    }
+    @PostMapping()
+
+     public ResponseEntity<ProductDto>crearProducto(@RequestHeader(name= "x-country", required = true)String country,
+                                                    @RequestHeader(name = "x-user",required = true)String user,
+                                                    @RequestBody ProductDto productDto){
+        log.info("Crear Producto Headers x-country:{}, x-user: {}", country,user); //los log son el Slf4j para identificar como el usuario y el pais
+        log.info("Crear Producto Request:{}, {}",productDto.toString(),"OK");
+        return ResponseEntity.ok(new ProductDto());
     }
 }
